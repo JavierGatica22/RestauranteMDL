e66123e09fe70799b6066e164998ec29b5409696
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 <ja.gaticas@duocuc.cl>
2022-07-10 19:53:45 -0400

    listos

--- ApiNodejs/config/conexion.js
+++ ApiNodejs/config/conexion.js
@@ -1,6 +1,6 @@
 const mysql2 = require('mysql2');
 const conexion = mysql2.createConnection({
-    host: '18.232.73.33',
+    host: '3.230.142.118',
     user: 'admin',
     password: 'javier123',
     port:3306,
